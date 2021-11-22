fd75255a62b7c5a6877f357e8467428b022f16a8
丁琪 <dingq@koal.com>
2021-11-22 11:28:38 +0800

    更新部署脚本

--- gitee-deploy.sh
+++ gitee-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# abort on errors
+# 确保脚本抛出遇到的错误
 set -e
 
 # build
@@ -9,17 +9,21 @@ yarn docs:build
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
-# if you are deploying to a custom domain
-echo 'http://gdut_yy.gitee.io/doc-onjava8/' > CNAME
+# 如果是发布到自定义域名
+# echo 'http://jadonding.github.io/doc-onjava8/' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@gitee.com:gdut_yy/doc-onjava8.git master
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
+# git push -f git@github.com:wangxifa/qinFeng.git master:gh-pages
+git push -f https://github.com/jadonding/OnJava8-zh.git master:gh-pages
 
 cd -
